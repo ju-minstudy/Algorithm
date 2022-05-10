8f7bdc35087d7e95bd8a6fcb409f612d873cf6de
Jupiter-J <namju2912@gmail.com>
2022-05-10 12:21:23 +0900

    Array

---
+++ Algorithm/src/Array_02/Q3/Re.java
@@ -0,0 +1,43 @@
+package Array_02.Q3;
+
+import java.util.Scanner;
+
+public class Re {
+    public static String solution(int n, int []a, int[]b){
+     String answer = "";
+
+        //A가 이겼을 경우를 모두 가정한다
+     for (int i=0; i<n; i++){
+
+         if (a[i] == b[i]){
+              answer += "D";
+         }else if(a[i] == 1 && b[i] == 3){
+              answer += "A";
+         }else if(a[i] ==2 && b[i] == 1){
+              answer += "A";
+         }else if (a[i] == 3 && b[i] == 2){
+              answer += "A";
+         }else
+              answer += "B";
+     }
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int []a = new int[n];
+        int []b = new int[n];
+
+        for (int i=0; i<n; i++){
+            a[i] = sc.nextInt();
+        }
+        for (int i=0; i<n; i++){
+            b[i] = sc.nextInt();
+        }
+
+     //   System.out.println(solution(n, a, b)); -> ABABD
+        for (char x : solution(n,a,b).toCharArray())
+            System.out.println(x);
+
+    }
+}
